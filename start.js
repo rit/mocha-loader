ce9adb8fd957f0a2b0f8f0b63e9fad370e1867fe
Rit Li <get@rit.li>
2017-05-21 11:09:23 -0700

    Run with an ended callback

--- start.js
+++ start.js
@@ -2,6 +2,8 @@ process.nextTick(function() {
 	delete require.cache[module.id];
 	if(typeof window !== "undefined" && window.mochaPhantomJS)
 		mochaPhantomJS.run();
+	else if (typeof window !== "undefined" && window.mochaEndedCallback)
+		mocha.run(window.mochaEndedCallback)
 	else
 		mocha.run();
 });
